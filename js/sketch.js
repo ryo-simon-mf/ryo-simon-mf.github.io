1fde9363b6b9ed8a5a376abdd04bf2d65c3fa08d
ryo-simon_mf <ryosimon1108@gmail.com>
2019-05-18 18:22:03 +0900

    fix sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -33,36 +33,4 @@ function draw() {
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-}let bn = 10 ;//box number
-let bs = 10;//box size
-let bm = 10;//box margin
-
-function setup() {
-    createCanvas(windowWidth, windowHeight,WEBGL);
-}
-
-function draw() {
-  // filter(ERODE);
-  background(255);
-  rotateX(radians(60));
-  rotateZ(frameCount / 150);
-  for(let i = 0; i < bn; i++){
-    for(let j = 0; j < bn; j++){
-      for(let k = 0; k < bn; k++){
-
-          push();
-          　translate((60 * i)-270,0);
-          　translate(0,(60 * j)-270);
-          　translate(0,0,(60 * k)-200)
-          　box(40+10*cos(frameCount/25));
-          pop();
-        }
-      }
-    }
-}
-
-
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
 }
